e751a5bae5c1f302a92035d8474a251cd7bb5d3f
itihon <kalabin.alexandr@gmail.com>
2025-06-25 17:46:05 +0500

    chore: add eslint

---
+++ eslint.config.js
@@ -0,0 +1,15 @@
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import { defineConfig } from "eslint/config";
+
+const files = [
+  "src/**/*.{js,mjs,cjs,ts,mts,cts}",
+  "test/**/*.{js,mjs,cjs,ts,mts,cts}",
+];
+
+export default defineConfig([
+  { files, plugins: { js }, extends: ["js/recommended"] },
+  { files, languageOptions: { globals: globals.browser } },
+  tseslint.configs.recommended.map(cfg => ({ ...cfg, files })),
+]);
