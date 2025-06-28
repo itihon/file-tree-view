ea0e975b60fad9b49f7c2e58f18cd457d2a51c36
itihon <kalabin.alexandr@gmail.com>
2025-06-28 19:07:30 +0500

    test: move html into fixture

--- cypress/e2e/file-tree-view/file-tree-view.cy.js
+++ cypress/e2e/file-tree-view/file-tree-view.cy.js
@@ -1,21 +1,23 @@
 import FileTreeView from '../../../src/index';
-import treeFixture from '../../fixtures/1-file-tree-view';
+import treeStructure from '../../fixtures/1-file-tree-view';
 
 describe('file-tree-view', () => {
-  it('has certain DOM structure', () => {
+  it('constructs certain DOM structure from loaded object structure', () => {
     cy.visit('localhost:5173');
+    cy.fixture('1-tree-structure.html').then(htmlStructure => {
+      
+      const ftView = new FileTreeView();
 
-    const ftView = new FileTreeView();
+      ftView.load(treeStructure);
 
-    ftView.load(treeFixture);
-
-    cy
-      .document()
-      .then(document => document.body.appendChild(ftView))
-      .then(() => {
-        cy
-          .get('file-tree-view')
-          .should('have.html', `<ftv-folder name="folder1"><ftv-auxiliary class="label noselect">folder1</ftv-auxiliary><ftv-auxiliary class="content"><ftv-file name="file1"><ftv-auxiliary class="label noselect">file1</ftv-auxiliary></ftv-file><ftv-folder name="folder2"><ftv-auxiliary class="label noselect">folder2</ftv-auxiliary><ftv-auxiliary class="content"><ftv-file name="file3"><ftv-auxiliary class="label noselect">file3</ftv-auxiliary></ftv-file><ftv-file name="file4"><ftv-auxiliary class="label noselect">file4</ftv-auxiliary></ftv-file><ftv-folder name="folder3"><ftv-auxiliary class="label noselect">folder3</ftv-auxiliary><ftv-auxiliary class="content"></ftv-auxiliary></ftv-folder></ftv-auxiliary></ftv-folder><ftv-folder name="folder4"><ftv-auxiliary class="label noselect">folder4</ftv-auxiliary><ftv-auxiliary class="content"><ftv-file name="file5"><ftv-auxiliary class="label noselect">file5</ftv-auxiliary></ftv-file><ftv-file name="file6"><ftv-auxiliary class="label noselect">file6</ftv-auxiliary></ftv-file><ftv-folder name="folder5"><ftv-auxiliary class="label noselect">folder5</ftv-auxiliary><ftv-auxiliary class="content"></ftv-auxiliary></ftv-folder></ftv-auxiliary></ftv-folder></ftv-auxiliary></ftv-folder>`);
-      });
+      cy
+        .document()
+        .then(document => document.body.appendChild(ftView))
+        .then(() => {
+          cy
+            .get('file-tree-view')
+            .should('have.html', htmlStructure);
+        });
+    });
   });
 });
