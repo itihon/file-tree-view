5b8f3104b6c727e2a03db2420881dbae000898a3
itihon <kalabin.alexandr@gmail.com>
2025-07-07 15:12:18 +0500

    chore(vite.config.js): configure vite-plugin-css-injected-by-js to be used in build mode

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,11 +1,17 @@
 import { defineConfig } from "cypress";
-import vitePreprocessor from 'cypress-vite'
+import vitePreprocessor from 'cypress-vite';
 
 export default defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
-      on('file:preprocessor', vitePreprocessor());
+      on(
+        'file:preprocessor', 
+        vitePreprocessor({ 
+          configFile: './vite.config.js', 
+          mode: 'development', 
+        }),
+      );
     },
   },
 });
