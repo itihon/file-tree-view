4b74bfe7d815438ac0ed5dae465968792ce7f416
itihon <kalabin.alexandr@gmail.com>
2025-07-23 17:16:20 +0500

    refactor(index.ts): expose necessary types

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,7 @@
 import FileTreeView from './FileTreeView.js';
 export default FileTreeView;
+
+import type FTVFile from './FTVFile.js';
+import type FTVFolder from './FTVFile.js';
+
+export type { FTVFile, FTVFolder };
