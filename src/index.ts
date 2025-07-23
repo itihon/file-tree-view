cb8124202125056ae849a60e88bea907df47b081
itihon <kalabin.alexandr@gmail.com>
2025-07-23 19:48:52 +0500

    fix(index.ts): correct type imports

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,6 @@ import FileTreeView from './FileTreeView.js';
 export default FileTreeView;
 
 import type FTVFile from './FTVFile.js';
-import type FTVFolder from './FTVFile.js';
+import type FTVFolder from './FTVFolder.js';
 
 export type { FTVFile, FTVFolder };
